102a22b02ef67dbfb912357626208c801e82241f
brayokenya <kiirubrian21@gmail.com>
2022-06-07 16:04:43 +0300

    REST API

--- news/urls.py
+++ news/urls.py
@@ -13,7 +13,9 @@ urlpatterns=[
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
     re_path(r'^new/article$', views.new_article, name='new-article'),
     re_path(r'^ajax/newsletter/$', views.newsletter, name='newsletter'),
-    re_path(r'^api/merch/$', views.MerchList.as_view())
+    re_path(r'^api/merch/$', views.MerchList.as_view()),
+    re_path(r'api/merch/merch-id/(?P<pk>[0-9]+)/$',
+        views.MerchDescription.as_view())
 
 ]
 if settings.DEBUG:
--- news/views.py
+++ news/views.py
@@ -130,3 +130,31 @@ class MerchList(APIView):
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
     permission_classes = (IsAdminOrReadOnly,)
+
+
+class MerchDescription(APIView):
+    permission_classes = (IsAdminOrReadOnly,)
+    def get_merch(self, pk):
+        try:
+            return MoringaMerch.objects.get(pk=pk)
+        except MoringaMerch.DoesNotExist:
+            return Http404
+
+    def get(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch)
+        return Response(serializers.data)
+
+    def put(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        serializers = MerchSerializer(merch, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        merch = self.get_merch(pk)
+        merch.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
